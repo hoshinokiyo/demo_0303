ed65301eb6ca87efb2daaa79052adf93356d9f1a
LAPTOP-DCG2OSMM\academia <202511_aw_hoshino@x-ide.sakura.ne.jp>
2026-03-03 10:46:52 +0900

    Section 5-2 Mapperインターフェースの作成

---
+++ src/main/java/com/example/demo/mapper/TodoMapper.java
@@ -0,0 +1,12 @@
+package com.example.demo.mapper;
+
+import java.util.List;
+
+import com.example.demo.model.Todo;
+
+public interface TodoMapper {
+
+    List<Todo> findAll();
+
+    int insert(Todo todo);
+}
