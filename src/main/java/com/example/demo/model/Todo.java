1c904d1ef3d580f5208dd1ae7a02316c84cbd71f
LAPTOP-DCG2OSMM\academia <202511_aw_hoshino@x-ide.sakura.ne.jp>
2026-03-03 10:43:36 +0900

    Section 5-1 Modelクラスの作成

---
+++ src/main/java/com/example/demo/model/Todo.java
@@ -0,0 +1,17 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Todo {
+
+    private Long id;
+    private String title;
+    private Boolean completed;
+}
