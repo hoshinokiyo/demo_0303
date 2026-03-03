d118daf3e6fe500cacc4dbf3b324afdca77c026b
LAPTOP-DCG2OSMM\academia <202511_aw_hoshino@x-ide.sakura.ne.jp>
2026-03-03 10:30:15 +0900

    Section 2-4 完了画面の作成

--- src/main/java/com/example/demo/TodoController.java
+++ src/main/java/com/example/demo/TodoController.java
@@ -32,6 +32,11 @@ public class TodoController {
         return "todo/confirm";
     }
 
+    @GetMapping("/complete")
+    public String complete() {
+        return "todo/complete";
+    }
+
     public record TodoItem(Long id, String title, String status) {
     }
 }
